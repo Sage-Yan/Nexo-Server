9c5db3863cdb75f58442d8718b386c0ebe5b5ae0
闫世杰 <shijieyan24@gmail.com>
2025-11-27 17:39:28 +0800

    init

---
+++ auth/src/main/java/com/sage/auth/AuthApplication.java
@@ -0,0 +1,17 @@
+package com.sage.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @classname AuthApplication
+ * @description 认证启动类
+ * @date 2025/11/27 17:08
+ * @created by YanShijie
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
---
+++ gateway/src/main/java/com/sage/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.sage.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @classname GatewayApplication
+ * @description 网关启动类
+ * @date 2025/11/27 17:06
+ * @created by YanShijie
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
